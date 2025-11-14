50fc9665f16ca7125eee91735fd42bc874493ab5
Daniel Jordan <danielmjordan.dev@gmail.com>
2025-11-14 14:57:32 -0700

    add api server, photos, convert to jsx

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -5,14 +5,20 @@ const App = () => {
   return (
     <div>
       <h1>Padre Gino's Pizza – Order Now</h1>
-      <Pizza name="Pepperoni" description="Mozzarella Cheese, Pepperoni" />
       <Pizza
-        name="The Hawaiian Pizza"
-        description="Sliced Ham, Pineapple, Mozzarella Cheese"
+        description="Mozzarella Cheese, Pepperoni"
+        image={"/public/pizzas/pepperoni.webp"}
+        name="Pepperoni"
       />
       <Pizza
-        name="The Big Meat Pizza"
+        description="Sliced Ham, Pineapple, Mozzarella Cheese"
+        image={"/public/pizzas/hawaiian.webp"}
+        name="The Hawaiian Pizza"
+      />
+      <Pizza
         description="Bacon, Pepperoni, Italian Sausage, Chorizo Sausage"
+        image={"/public/pizzas/big_meat.webp"}
+        name="The Big Meat Pizza"
       />
     </div>
   );
