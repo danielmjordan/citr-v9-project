92344b5e9b74ca07ff859a1e9292103f09ec40ec
Daniel Jordan <danielmjordan.dev@gmail.com>
2025-11-14 15:08:23 -0700

    add form

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,25 +1,11 @@
 import { createRoot } from "react-dom/client";
-import Pizza from "./Pizza";
+import Order from "./Order";
 
 const App = () => {
   return (
     <div>
       <h1>Padre Gino's Pizza – Order Now</h1>
-      <Pizza
-        description="Mozzarella Cheese, Pepperoni"
-        image={"/public/pizzas/pepperoni.webp"}
-        name="Pepperoni"
-      />
-      <Pizza
-        description="Sliced Ham, Pineapple, Mozzarella Cheese"
-        image={"/public/pizzas/hawaiian.webp"}
-        name="The Hawaiian Pizza"
-      />
-      <Pizza
-        description="Bacon, Pepperoni, Italian Sausage, Chorizo Sausage"
-        image={"/public/pizzas/big_meat.webp"}
-        name="The Big Meat Pizza"
-      />
+      <Order />
     </div>
   );
 };
