5b4b41e97508e8e26e06a1942f369267320d9b29
Daniel Jordan <danielmjordan.dev@gmail.com>
2025-11-15 09:29:49 -0700

    update directory structure, add debugging logic

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,11 +1,13 @@
 import { createRoot } from "react-dom/client";
-import Order from "./Order";
+import Order from "./components/Order";
+import PizzaOfTheDay from "./partials/PizzaOfTheDay";
 
 const App = () => {
   return (
     <div>
       <h1>Padre Gino's Pizza – Order Now</h1>
       <Order />
+      <PizzaOfTheDay />
     </div>
   );
 };
